e5400ae97a2ba4ab23ea07c1ec6355ac5df55c8d
Luca Cotugno <cotugno.luca@virgilio.it>
2023-01-19 12:53:26 +0100

    Commit

---
+++ Android Project/app/src/main/java/com/example/multiroomlocalization/CropView.java
@@ -0,0 +1,45 @@
+package com.example.multiroomlocalization;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+
+import com.canhub.cropper.CropImageView;
+
+public class CropView extends Dialog {
+    private Uri inputImage;
+    private Button salva;
+    private Uri outputImage;
+    private CropImageView cropImageView;
+
+    public CropView(@NonNull Context context,Uri inputImage) {
+        super(context);
+        this.inputImage = inputImage;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.crop_view);
+        cropImageView = findViewById(R.id.cropImageView);
+        salva = findViewById(R.id.salvaCrop);
+        cropImageView.setImageUriAsync(inputImage);
+        cropImageView.setAspectRatio(1440, 2308);
+    }
+
+    protected void setTouchButton(View.OnTouchListener touch){
+        salva.setOnTouchListener(touch);
+    }
+    protected CropImageView getCropImageView(){
+        return this.cropImageView;
+    }
+
+}
--- Android Project/app/src/main/java/com/example/multiroomlocalization/MainActivity.java
+++ Android Project/app/src/main/java/com/example/multiroomlocalization/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.multiroomlocalization;
 
+import android.Manifest;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.bluetooth.BluetoothA2dp;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -11,13 +11,11 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.os.Bundle;
 
 import android.os.Handler;
-import android.os.IBinder;
 import android.os.Parcelable;
 import android.support.v4.media.MediaBrowserCompat;
 import android.support.v4.media.MediaMetadataCompat;
@@ -26,7 +24,6 @@ import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
 import android.util.Log;
 import android.view.View;
-import android.widget.AbsSeekBar;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -40,6 +37,38 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import java.util.Set;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+
+import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.multiroomlocalization.databinding.ActivityMainBinding;
+//import com.yalantis.ucrop.UCrop;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewTreeObserver;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -57,6 +86,24 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView playPause;
     private SeekBar audioSeekBar;
+
+    private ActivityMainBinding binding;
+    private Handler mHandler = new Handler();
+    private ScanService scanService;
+    private Canvas canvas;
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private EditText labelRoom;
+    private Button cancel, save, upload;
+    private ImageView imageView;
+    private Bitmap mutableBitmap;
+    private int imageViewHeight;
+    private int imageViewWidth;
+    private boolean first = true;
+    private boolean newImage = true;
+    private int intervalScan = 10000;
+
+    private ArrayList<ReferencePoint> referencePoints = new ArrayList<ReferencePoint>();
 
     private final MediaBrowserCompat.ConnectionCallback connectionCallbacks =
             new MediaBrowserCompat.ConnectionCallback() {
@@ -156,13 +203,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
-        Button scanBT = (Button) findViewById(R.id.scanBT);
-        scanBT.setOnClickListener(askBtPermission);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION,1 );
+                checkPermission(Manifest.permission.ACCESS_FINE_LOCATION, 1);
+            }
+        });
+
+        //Button scanBT = (Button) findViewById(R.id.scanBT);
+        //scanBT.setOnClickListener(askBtPermission);
         activity = this;
 
-        audioSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        //audioSeekBar = (SeekBar) findViewById(R.id.seekBar);
         audioSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int pos, boolean fromUser) {}
@@ -186,6 +246,286 @@ public class MainActivity extends AppCompatActivity {
                 connectionCallbacks,
                 null);
 
+
+        //DA RIVEDERE
+
+
+
+
+        imageView = (ImageView) findViewById(R.id.map);
+
+        imageView.getViewTreeObserver().addOnGlobalLayoutListener( new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                imageViewHeight = imageView.getHeight();
+                imageViewWidth = imageView.getWidth();
+                int xGlobal = imageView.getLeft();
+                int yGlobal = imageView.getTop();
+
+                System.out.println("Global");
+                System.out.println("X: " + xGlobal);
+                System.out.println("Y: " + yGlobal);
+                System.out.println("Height: " + imageViewHeight + " Width: " + imageViewWidth);
+                // don't forget to remove the listener to prevent being called again
+                // by future layout events:
+                if(first || newImage) {
+                    first=false;
+                    newImage = false;
+                    Bitmap bitmap = Bitmap.createScaledBitmap(((BitmapDrawable) imageView.getDrawable()).getBitmap(), imageViewWidth, imageViewHeight, true);
+                    mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+                    canvas = new Canvas(mutableBitmap);
+                }
+            }
+        });
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int x1 = (int) event.getX();
+                int y1 = (int) event.getY();
+
+                System.out.println("X: " + x1 + " Y: " + y1);
+
+                int tempx = x1;
+                int tempy = y1;
+
+                createDialog(tempx,tempy);
+                return false;
+            }
+        });
+
+    }
+
+    private Runnable scanRunnable = new Runnable() {
+        @Override
+        public void run() {
+            scanService.startScan();
+            mHandler.postDelayed(scanRunnable, intervalScan);
+        }
+    };
+
+    private void stopScan(){
+        mHandler.removeCallbacks(scanRunnable);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            dialogBuilder = new AlertDialog.Builder(this);
+            final View popup = getLayoutInflater().inflate(R.layout.popup_upload_map, null);
+            upload = (Button) popup.findViewById(R.id.upload);
+
+            dialogBuilder.setView(popup);
+            dialog = dialogBuilder.create();
+            dialog.show();
+
+            upload.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    // create an instance of the
+                    // intent of the type image
+                    Intent i = new Intent();
+                    i.setType("image/*");
+                    i.setAction(Intent.ACTION_GET_CONTENT);
+
+                    someActivityResultLauncher.launch(i);
+                }
+            });
+
+
+            System.out.println("Presss");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // You can do the assignment inside onAttach or onCreate, i.e, before the activity is displayed
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        Uri image = data.getData();
+                        //Uri destImage = null;
+
+                        if (null != image) {
+
+                            /*UCrop.of(image,destImage)
+                                    .withAspectRatio(9,16)
+                                    .withMaxResultSize(1080,1920)
+                                    .start(MainActivity.this);
+                            */
+
+                            dialog.cancel();
+                            CropView cv = new CropView(MainActivity.this, image);
+                            cv.setCanceledOnTouchOutside(false);
+                            cv.show();
+                            cv.setTouchButton(new View.OnTouchListener() {
+                                @Override
+                                public boolean onTouch(View view, MotionEvent motionEvent) {
+                                    String path = MediaStore.Images.Media.insertImage(getApplicationContext().getContentResolver(), cv.getCropImageView().getCroppedImage(), "Title", null);
+
+                                    imageView.setImageURI(Uri.parse(path));
+                                    newImage=true;
+
+                                    cv.cancel();
+                                    return false;
+                                }
+                            });
+                        }
+
+
+                    }
+                }
+            });
+
+/*    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            final Uri resultUri = UCrop.getOutput(data);
+            imageView.setImageURI(resultUri);
+            newImage=true;
+
+            System.out.println("DIMENSION BitmaP: " +   mutableBitmap.getHeight() + " " + mutableBitmap.getWidth());
+            System.out.println("DIMENSION BitmaPScaled: " + mutableBitmap.getScaledHeight(canvas) + " " + mutableBitmap.getScaledWidth(canvas));
+            System.out.println("DIMENSION: " + imageViewHeight + " " + imageViewWidth);
+
+            System.out.println("REFERENCE: " + referencePoints);
+
+            referencePoints.clear();
+            System.out.println("REFERENCE: " + referencePoints);
+
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            final Throwable cropError = UCrop.getError(data);
+        }
+    }
+*/
+
+    public void checkPermission(String permission, int requestCode)
+    {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] { permission }, requestCode);
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+            scanService = new ScanService(getApplicationContext());
+
+            mHandler.postDelayed(scanRunnable, intervalScan);
+        }
+    }
+
+    // This function is called when the user accepts or decline the permission.
+    // Request Code is used to check which permission called this function.
+    // This request code is provided when the user is prompt for permission.
+ /*   @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode,
+                permissions,
+                grantResults);
+
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Camera Permission Granted", Toast.LENGTH_SHORT) .show();
+                scanService = new ScanService(getApplicationContext());
+
+                mHandler.postDelayed(scanRunnable, intervalScan);
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Camera Permission Denied", Toast.LENGTH_SHORT) .show();
+            }
+        }
+        else if (requestCode == 0) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Storage Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+*/
+    private void createDialog(int x,int y){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View popup = getLayoutInflater().inflate(R.layout.popup_room, null);
+        labelRoom = (EditText) popup.findViewById(R.id.labelRoom);
+        cancel = (Button) popup.findViewById(R.id.buttonCancel);
+        save = (Button) popup.findViewById(R.id.buttonSave);
+
+        save.setEnabled(false);
+
+        dialogBuilder.setView(popup);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        labelRoom.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if(charSequence.toString().trim().length()==0){
+                    save.setEnabled(false);
+                } else {
+                    save.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Paint mPaint = new Paint();
+                mPaint.setColor(Color.RED);
+                canvas.drawCircle(x, y, 20, mPaint);
+                imageView.setImageDrawable(new BitmapDrawable(getResources(), mutableBitmap));
+                ReferencePoint ref = new ReferencePoint(x,y,labelRoom.getText().toString());
+                referencePoints.add(ref);
+                Toast.makeText(getApplicationContext(), "Stanza aggiunta correttamente", Toast.LENGTH_LONG).show();
+                dialog.cancel();
+            }
+        });
     }
 
     @Override
@@ -326,13 +666,22 @@ public class MainActivity extends AppCompatActivity {
 
         switch (requestCode) {
             case BT_CONNECT_AND_SCAN:
-
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
                     Toast.makeText(this, "BT Permission Granted", Toast.LENGTH_SHORT).show();
                     launchAssignRAFragment();
                 } else {
                     Toast.makeText(this, "BT Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            case 1: //DEFINIRE CODICE RICHIESTA
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "Camera Permission Granted", Toast.LENGTH_SHORT) .show();
+                    scanService = new ScanService(getApplicationContext());
+
+                    mHandler.postDelayed(scanRunnable, intervalScan);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
                 }
                 return;
         }
